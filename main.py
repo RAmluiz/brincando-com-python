9f8e4439ce770fcac76878d1094ed9ee51dadd8a
RAmluiz <raphael.amaralluiz@gmail.com>
2020-06-09 00:00:31 -0300

    mais um pedaço

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print("Hello World")
+empty_dic = {}
+d1 = {'a': "soma value", "b":[1,2,3,4]}
+d1
+
+
+
+
+# print("Hello World")
+
+# print("Teste")
+# a = (1,2,2,2,3,4,2)
+
+# print(a)
+
+# print(a.count(2))
+
+# gen = range(0,150,1)
+# listta = list(range(2,15,1))      
+# print(listta)            
